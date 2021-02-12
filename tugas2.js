40d6a754c46c27127d27492ddb3d610cb837e7dd
Herik Suseno <>
2021-02-12 13:43:59 +0700

    Tugas 2 JS untuk NIOMIC

---
+++ tugas2.js
@@ -0,0 +1,13 @@
+var angka = 30;
+var namalengkap ="Nama Saya Herik Suseno";
+var benar = true;
+var kosong = null;
+var tidakada;
+var deretan =[2,1,0,1,1,9,9,1];
+
+console.log(angka)
+console.log(namalengkap)
+console.log(benar)
+console.log(kosong)
+console.log(tidakada)
+console.log(deretan[0])
